92ee4cf7b0daa9546b275bd17bc9e0300bf4c87d
jichong-nj <jichong79@gmail.com>
2026-05-12 09:16:01 +0800

    改成pg_jieba

--- postgres/initdb/01-init-all.sql
+++ postgres/initdb/01-init-all.sql
@@ -4,12 +4,12 @@
 -- 1. 创建 pgvector 扩展
 CREATE EXTENSION IF NOT EXISTS vector;
 
--- 2. 创建 zhparser 中文分词扩展
-CREATE EXTENSION IF NOT EXISTS zhparser;
+-- 2. 创建 pg_jieba 中文分词扩展
+CREATE EXTENSION IF NOT EXISTS pg_jieba;
 
--- 3. 使用 zhparser 创建中文文本搜索配置
-CREATE TEXT SEARCH CONFIGURATION IF NOT EXISTS chinese (PARSER = zhparser);
+-- 3. 使用 pg_jieba 创建中文文本搜索配置
+CREATE TEXT SEARCH CONFIGURATION IF NOT EXISTS chinese (PARSER = jieba);
 
 -- 4. 配置中文分词映射
 ALTER TEXT SEARCH CONFIGURATION chinese 
-    ADD MAPPING FOR n,v,a,i,e,l WITH simple;
+    ADD MAPPING FOR n,v,a,i,e,l,x WITH jieba;
