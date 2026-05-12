27bbe71f1e03a7bac31938f0d0cc6e30bfd638ca
jichong-nj <jichong79@gmail.com>
2026-05-12 12:24:28 +0800

    改回zhparser

--- postgres/initdb/01-init-pgvector.sql
+++
@@ -1,5 +0,0 @@
--- 初始化 pgvector 扩展
-CREATE EXTENSION IF NOT EXISTS vector;
-
--- 创建中文文本搜索配置（基于 simple，因为 pgvector 镜像可能没有 jieba）
-CREATE TEXT SEARCH CONFIGURATION IF NOT EXISTS chinese (COPY = simple);
